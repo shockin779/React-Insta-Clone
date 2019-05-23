813a80bb94e870b635bae7c580322a00af10812e
shockin779 <sean.hockin@gmail.com>
2019-05-23 12:21:19 -0600

    Created post container and styling

---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './PostContainer.css';
+
+const PostContainer = props => {
+    return (
+        <main className='posts-section'>
+            <div className='post'>
+                <div className='author'>
+                    <img src='https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg'></img>
+                    <p>philzcoffee</p>
+                </div>
+                <img className='post-image' src='https://tk-assets.lambdaschool.com/69cf901b-f96d-466e-a745-ff2a01effac9_philz-image.jpg'></img>
+                <div className='post-icons'>
+                    <i class="far fa-heart"></i>
+                    <i class="far fa-comment"></i>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default PostContainer;
