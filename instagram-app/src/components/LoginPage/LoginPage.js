df49f958ac21e4802a2d00c9c1a9aaf695cbad3f
Sean Hockin <sean.hockin@gmail.com>
2019-06-03 21:51:04 -0600

    Created Login System

---
+++ instagram-app/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './LoginPage.css';
+
+class LoginPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleInputUpdate = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleLoginSubmit = event => {
+        const user = this.state.username;
+        localStorage.setItem('user', user);
+        window.location.reload();
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.handleLoginSubmit}className='login-form'>
+                <h3>Welcome to React Insta Clone</h3>
+                <div>Please Login</div>
+                <input name='username' onChange={this.handleInputUpdate} type='text' placeholder='Username'></input>
+                <input name='password' onChange={this.handleInputUpdate} type='password' placeholder='Password'></input>
+                <button onClick={this.handleLoginSubmit} value='Login'></button>
+            </form>
+        )
+    }
+}
